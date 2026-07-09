a3108a4eb9aeb3e2a28e40696a7d1d16dfce83d3
Natan <nr020775@gmail.com>
2026-07-09 17:17:47 -0300

    feat: adicionando exercicio

--- src/ex1/Main.java
+++ src/ex1/Main.java
@@ -1,7 +1,30 @@
 package ex1;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        // 1 - Faça um algoritmo que leia os valores de A, B, C e em seguida imprima na tela a soma entre A e B e mostre se a soma é menor que C.
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Entre com um valor para A");
+        double a = sc.nextDouble();
+
+        System.out.println("Entre com um valor para B");
+        double b = sc.nextDouble();
+
+        System.out.println("Entre com um valor para C");
+        double c = sc.nextDouble();
+
+
+        double soma = a + b;
+        System.out.println("A soma entre A & B é: " + soma);
+        if (soma < c){
+            System.out.println("O resultado é menor que C: C = " + c);
+        }
 
     }
 }
---
+++ src/ex2/Main.java
@@ -0,0 +1,27 @@
+package ex2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 2 - Faça um algoritmo para receber um número qualquer e imprimir na tela se o número é par ou ímpar, positivo ou negativo.
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Entre com um numero");
+        int number = sc.nextInt();
+
+
+        if ( number > 0 && number % 2 == 0){
+            System.out.println("O numero é par e positivo, numero é " + number);
+        }else if (number < 0 && number % 2 != 0){
+            System.out.println("O numero é impar e negativo, numero é " + number);
+        } else if (number > 0 && number % 2 != 0) {
+            System.out.println("O numero é impar e positivo, numero é " + number);
+        } else if (number < 0 && number % 2 == 0) {
+            System.out.println("O numero é par e negativo, numero é " + number);
+        }else {
+            System.out.println("O numero não pode ser 0");
+        }
+    }
+}
